95bb789518193e66575215d92622f7f67bf37ff2
Tim Pinkerton <timpinkerton@mail.com>
2018-01-04 19:54:53 -0500

    * Artists.cs: Added Artists model
    * ComicBook.cs: Added ComicBooks model

---
+++ Models/ComicBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicbook.Models
+{
+    public class ComicBook
+    {
+        //this is a property to identify a comic book
+        public int Id { get; set; }
+        //This property represents the title of the series 
+        //that the comic book is a part of
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        //Artists property is an array of Artists
+        public Artists[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
